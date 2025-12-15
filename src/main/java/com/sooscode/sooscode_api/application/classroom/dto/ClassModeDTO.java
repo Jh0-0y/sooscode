41ff069c5702d7300340534900485c4f39208252
subin <sooxpak@gmail.com>
2025-12-15 15:47:11 +0900

    [feat]class: 읽기모드, 자유모드

---
+++ src/main/java/com/sooscode/sooscode_api/application/classroom/dto/ClassModeDTO.java
@@ -0,0 +1,6 @@
+package com.sooscode.sooscode_api.application.classroom.dto;
+
+public class ClassModeDTO {
+    private String mode; // "VIEW_ONLY", "FREE_PRACTICE", "QUIZ"
+    private String timestamp;
+}
