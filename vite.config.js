eb9690d0fd1dfdc565cdf71599390d52588874dd
jhy <idpc8090@gmail.com>
2025-12-17 21:11:45 +0900

    [refactor] api

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        host: true,
+        allowedHosts: ["sooscode.kro.kr"],
+    },
+    plugins: [react()],
     define: {
-        global: 'globalThis',
+        global: "globalThis",
     },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
     },
-  },
 });
